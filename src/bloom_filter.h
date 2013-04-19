27d64eed5c76ffb1cfd690d5a81d182eb2448138
demon <magiczhao@yahoo.cn>
2013-04-19 12:55:13 +0800

    Update bloom_filter.h

--- src/bloom_filter.h
+++ src/bloom_filter.h
@@ -1,5 +1,9 @@
 #ifndef _DEVLIB_BLOOM_FILTER_H
 #define _DEVLIB_BLOOM_FILTER_H
+/**
+ * simple implementation of bloom filter
+ * the theory is http://billmill.org/bloomfilter-tutorial/
+ */
 #include "hash_func.h"
 #include "common.h"
 static dhash_func hfuncs[] = {
@@ -18,7 +22,10 @@ struct dbloom_filter
     char* buffer;
 };
 
+/** counter bloom filter fail, to caculate fail rate */
 #define mark_fail(bf)   do{(bf)->fail_count += 1;}while(0)
+
+/** init a bloom filter */
 int dbloom_filter_init(struct dbloom_filter*, int capacity, int hash_count);
 void dbloom_filter_fini(struct dbloom_filter*);
 void dbloom_filter_add(struct dbloom_filter*, const char* value, unsigned int size);
