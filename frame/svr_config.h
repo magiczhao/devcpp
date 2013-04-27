3954fefb8df10036a3d7510c446a7f3c06c719bc
magiczhao <magiczhao@yahoo.cn>
2013-04-27 22:17:07 +0800

    remove temp defines

--- frame/svr_config.h
+++ frame/svr_config.h
@@ -2,19 +2,12 @@
 #define _DEVLIB_FRAME_CONFIG_H
 #include "common.h"
 
-#define CONF_DEFINE(tn, def)     \
+#define TCC_DEFINE(tn, def)     \
     struct tn                    \
         def;                     \
     const static char tn ## _def_string[] = "struct " #tn #def ";";
 
-/*
-CONF_DEFINE(frame_config, {
-    char listen_ip[16];
-    char run_dir[256];
-    short listen_port;
-    int threads;
-})*/
-CONF_DEFINE(frame_config, {
+TCC_DEFINE(frame_config, {
     char* listen_ip;
     char* run_dir;
     short listen_port;
