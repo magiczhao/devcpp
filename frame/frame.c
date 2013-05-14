fa18556eeacce6ee398f9a5ea45b1a4ce98047cf
magiczhao <magiczhao@yahoo.cn>
2013-05-14 18:01:37 +0800

    add thread here

--- frame/frame.c
+++ frame/frame.c
@@ -3,11 +3,17 @@
 #include "svr_log.h"
 #include "iohelper.h"
 #include "common.h"
+#include "dlock.h"
 #include <unistd.h>
 #include <getopt.h>
+#include <signal.h>
 #include <arpa/inet.h>
+#include <sys/types.h>
+#include <sys/wait.h>
 
+int is_stop = false;
 struct frame_config svrconf;
+dlock_t thread_lock;
 int init_svr_config(const char* file)
 {
     if(init_config(&svrconf) != 0){
@@ -67,12 +73,55 @@ fail:
     }
     return -1;
 }
+void handle_sigint(int sig)
+{
+    is_stop = true;
+}
+
+void handle_child(int sig)
+{
+    pid_t child;
+    int status;
+    while((child = waitpid(-1, &status, WNOHANG)) > 0);
+}
+
+int setup_signal()
+{
+    //TODO setup signal functions
+    if(SIG_ERR == signal(SIGINT, handle_sigint)){
+        error("set SIGINT handler failed:%s", strerror(errno));
+        return -1;
+    }
+    if(SIG_ERR == signal(SIGPIPE, SIG_IGN)){
+        error("set SIGPIPE handler failed:%s", strerror(errno));
+        return -1;
+    }
+    
+    if(SIG_ERR == signal(SIGCHLD, handle_child)){
+        error("set SIGCHLD handler failed:%s", strerror(errno));
+        return -1;
+    }
+    info("setup signals ok!");
+    return 0;
+}
+
+void* thread_loop(void* arg)
+{
+    struct event_base* base;
+    base = event_base_new();
+    if(!base){
+        error("init event base failed!");
+        return (void*) -1;
+    }
+    return NULL;
+}
 
 int main(int argc, char** argv)
 {
     int is_daemon = 0;
     char conf_file[DPATH_MAX] = {0};
     char ch;
+    pthread_t* workers = NULL;
     while((ch = getopt(argc, argv, "c:d")) != -1){
         switch(ch){
             case 'c':
@@ -105,6 +154,28 @@ int main(int argc, char** argv)
         return -1;
     }
     info("socket listened!");
+    if(dlock_init(&thread_lock) != 0){
+        error("init lock failed:%s", strerror(errno));
+        return -1;
+    }
+    if(svrconf.threads > 0){
+        workers = (pthread_t*) malloc(sizeof(pthread_t) * svrconf.threads);
+        for(int i = 0; i < svrconf.threads; ++i){
+            if(pthread_create(workers + i, NULL, thread_loop, NULL) != 0){
+                error("dispatch thread failed:%s", strerror(errno));
+                is_stop = true;
+                return -1;
+            }
+        }
+    }else{
+        //create only one thread
+        workers = (pthread_t*) malloc(sizeof(pthread_t));
+        if(pthread_create(workers, NULL, thread_loop, NULL) != 0){
+            error("dispatch thread failed:%s", strerror(errno));
+            is_stop = true;
+            return -1;
+        }
+    }
     fini_config(&svrconf);
     return 0;
 }
