1bb024cb0bab3bbe38d28c1cfbc3e7ba24fa2b05
demon <magiczhao@yahoo.cn>
2013-05-29 16:16:58 +0800

    Update dlop.h

--- frame/dlop.h
+++ frame/dlop.h
@@ -2,21 +2,28 @@
 #define DEVLIB_FRAME_DLOP_H
 #include <dlfcn.h>
 struct connection;
+/** client event callback function */
 typedef int(*handle_func)(int sock, struct connection* conn);
+/** network package process callback function */
 typedef int(*handle_buffer)(const char* buffer, int size);
+
 struct frame_callback
 {
+    /** dynamic library handle */
     void *lib_handle;
-    handle_func read_cb;
-    handle_func write_cb;
-    handle_func init_cb;
-    handle_func fini_cb;
-    handle_func timeout_cb;
+    /** callback functions here */
+    handle_func read_cb; //calls on read event
+    handle_func write_cb; //calls on write event
+    handle_func init_cb; //calls on client connect
+    handle_func fini_cb; //calls on client close
+    handle_func timeout_cb; //calls on timeout
 };
 
 struct net_util
 {
+    /** dynamic library handle */
     void* lib_handle;
+    /** call on recv, check if the package finish */
     handle_buffer package_complete;
 };
 
